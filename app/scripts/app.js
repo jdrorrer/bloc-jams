a6ee1ea2645ff7dbed3c7184871f7da07d65d86d
Jake Rorrer <jdrorrer@gmail.com>
2015-02-02 22:58:34 +0000

    Added volume control

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -124,6 +124,26 @@ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope
 
 blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
   $scope.songPlayer = SongPlayer;
+  var previousVolume = SongPlayer.volume;
+
+  $scope.volumeClass = function() {
+    return {
+      'fa-volume-off': SongPlayer.volume == 0,
+      'fa-volume-down': SongPlayer.volume <= 70 && SongPlayer.volume > 0,
+      'fa-volume-up': SongPlayer.volume > 70
+    }
+  }
+
+  $scope.volumeMute = function() {
+    if(SongPlayer.volume == 0) {
+      SongPlayer.setVolume(previousVolume);
+
+    }
+    else {
+      previousVolume = SongPlayer.volume;
+      SongPlayer.setVolume(0);
+    }
+  }
 
   SongPlayer.onTimeUpdate(function(event, time) {
     $scope.$apply(function() {
@@ -143,6 +163,7 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     currentSong: null,
     currentAlbum: null,
     playing: false,
+    volume: 90,
 
     play: function() {
       this.playing = true;
@@ -180,6 +201,24 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
     onTimeUpdate: function(callback) {
       return $rootScope.$on('sound:timeupdate', callback);
     },
+    unmute: function() {
+      if(currentSoundFile) {
+        currentSoundFile.unmute();
+      }
+      var currentVolume = currentSoundFile.getVolume();
+      this.setVolume(currentVolume);
+    },
+    getVolume: function() {
+      if(currentSoundFile) {
+        currentSoundFile.getVolume();
+      }
+    },
+    setVolume: function(volume) {
+      if(currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+      }
+      this.volume = volume;
+    },
     setSong: function(album, song) {
       if (currentSoundFile) {
         currentSoundFile.stop();
@@ -191,6 +230,8 @@ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
         formats: [ "mp3" ],
         preload: true
       });
+
+      currentSoundFile.setVolume(this.volume);
 
       currentSoundFile.bind('timeupdate', function(e) {
         $rootScope.$broadcast('sound:timeupdate', this.getTime());
